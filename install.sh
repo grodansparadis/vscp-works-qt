1b23a60b9ca60f265a688f1fc5bde076ccbb4ecd
Ake <akhe@grodansparadis.com>
2021-03-09 14:12:23 +0100

    Test

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ sed -e "s,FULLPATH,$PWD,g" install/vscp-works-qt.desktop > vscp-works-qt.desktop
 cp vscp-works-qt.desktop.temp ~/.local/share/applications/vscp-works-qt.desktop
 cp vscp-works-qt.desktop.temp ~/Desktop/vscp-works-qt.desktop
 rm vscp-works-qt.desktop.temp
-#cp install/vscp-works-qt.desktop ~/.local/share/applications/vscp-works-qt.desktop
-#cp install/vscp-works-qt.desktop ~/Desktop/vscp-works-qt.desktop
+cp install/vscp-works-qt.desktop ~/.local/share/applications/vscp-works-qt.desktop
+cp install/vscp-works-qt.desktop ~/Desktop/vscp-works-qt.desktop
 #rm install/vscp-works-qt.desktop
 
 echo "Installed vscp-works-qt icons on menu and desktop !"
