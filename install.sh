16343155b26cbe71df8696b81502e8f243512963
Ake <akhe@grodansparadis.com>
2021-03-09 14:20:00 +0100

    test

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 CURDIR=`pwd`
 APPDIR="$(dirname -- "$(readlink -f -- "${0}")" )"
 
-cd "$APPDIR"
+//cd "$APPDIR"
 
 # Set absolute path work around
-sed -e "s,FULLPATH,$PWD,g" install/vscp-works-qt.desktop > vscp-works-qt.desktop.temp
+//sed -e "s,FULLPATH,$PWD,g" install/vscp-works-qt.desktop > vscp-works-qt.desktop.temp
 
 cp vscp-works-qt.desktop.temp ~/.local/share/applications/vscp-works-qt.desktop
 cp vscp-works-qt.desktop.temp ~/Desktop/vscp-works-qt.desktop
